d4626de2682f5a3bd5f191dbde14f8bedd12301f
muxuejin <muxuejin@outlook.com>
2025-05-08 21:59:16 +0800

    删除升级提示与去广告

--- flutter/lib/desktop/pages/desktop_setting_page.dart
+++ flutter/lib/desktop/pages/desktop_setting_page.dart
@@ -2117,7 +2117,7 @@ class _AboutState extends State<_About> {
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text(
-                            'Copyright © ${DateTime.now().toString().substring(0, 4)} Purslane Ltd.\n$license',
+                            'Copyright © ${DateTime.now().toString().substring(0, 4)} Purslane Ltd. @Muxuejin\n$license',
                             style: const TextStyle(color: Colors.white),
                           ),
                           Text(
